a7090f895b7cb7efb66613d95155b7d8500b2a12
Jack Maney <jackmaney@gmail.com>
2015-11-20 10:00:57 -0600

    Added a test for default config files

---
+++ test/test4_default_file.py
@@ -0,0 +1,51 @@
+import sys
+
+sys.path = ['..', '.'] + sys.path
+
+import unittest
+import clickfig
+import os
+from dict_equal import dict_equal
+
+
+class TestDefaultFile(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cfg_ini = clickfig.ConfigFile("./default_test.ini", default_file="./ini/default.ini",
+                                          verbose=False)
+        cls.cfg_json = clickfig.ConfigFile("./default_test.json",
+                                           config_type="json", default_file="./json/default.json",
+                                           verbose=False)
+
+    def test_ini(self):
+        expected = {
+            "first": {
+                "foo": "bar",
+                "baz": "3"
+            },
+            "second": {
+                "blarg": "something something\nsomething"
+            }
+        }
+
+        self.assertTrue(self.cfg_ini.exists())
+        self.assertTrue(dict_equal(self.cfg_ini.read(flatten=False).data, expected))
+
+    def test_json(self):
+        expected = {
+            "first": {
+                "foo": "bar",
+                "baz": 3
+            },
+            "second": {
+                "blarg": "something something\nsomething"
+            }
+        }
+
+        self.assertTrue(self.cfg_json.exists())
+        self.assertTrue(dict_equal(self.cfg_json.read(flatten=False).data, expected))
+
+    @classmethod
+    def tearDownClass(cls):
+        os.unlink(cls.cfg_ini.config_file)
+        os.unlink(cls.cfg_json.config_file)
