668bb57af57279ad1ae8a8ccd4405adef35b2793
Jack Maney <jackmaney@gmail.com>
2016-02-25 15:50:32 -0600

    0.3.0: --unset method added. Slight refactor

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
-from .clickfig import __version__
+from clickfig import version
 
 try:
     with open("requirements.txt") as f:
         requirements = [x.strip() for x in f if x.strip()]
 except IOError as e:
     requirements = []
-
+print("requirements = {}".format(requirements))
 setup(
     name='clickfig',
-    version=__version__,
-    packages=find_packages,
+    version=version.__version__,
+    packages=find_packages(),
     url='',
     license='MIT',
     author='Jack Maney',
